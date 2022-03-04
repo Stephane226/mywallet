a4d7693bbdf565ee5d6864f4b5b4706cbb407f44
Masaaki Stephane <bstephane97@yandex.com>
2022-03-04 17:39:26 +0300

    different components UI has been created
    Header
    Balance
    TransactionList
    incomeExpensive.js
     AddTransaction

--- wallet/src/App.js
+++ wallet/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Header} from './components/header.js';
+import {Balance} from './components/balance.js';  
+import TransactionList from './components/transactionList'; 
+import IncomeExpensive from './components/incomeExpensive.js'; 
+import  AddTransaction from './components/addTransaction';  
+ 
 function App() {
   return (
-    <div className="App">
-     hellow
+    <div>
+     <Header/>
+     <div className="container">
+     <Balance/>
+     <IncomeExpensive/>
+     <TransactionList/>
+     <AddTransaction/>
+    </div>
+   
     </div>
   );
 }
--- wallet/src/components/header.js
+++ wallet/src/components/header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const header = () => {
+export const Header = () => {
   return (
-    <div>header</div>
+    <div>
+        Expenses Tracker
+    </div>
   )
 }
